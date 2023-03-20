c5b33da97ddd6dfe58e94e11536d343cb44a9756
maksimilyan.lamaev.13@inbox.ru <maksimilyan.lamaev.13@inbox.ru>
2023-03-20 11:25:50 +0700

    0

---
+++ PZ_10_ex1/ColorRGB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_10_ex1
+{
+    public class ColorRGB
+    {
+        public byte red { get; set; }
+        public byte green { get; set; }
+        public byte blue { get; set; }
+
+        public ColorRGB mcolor { get; set; }
+        public System.Windows.Media.Color clr { get; set; }
+    }
+}
--- PZ_10_ex1/MainWindow.xaml.cs
+++ PZ_10_ex1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -21,9 +22,17 @@ namespace PZ_10_ex1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ColorRGB mcolor;
+        private Color clr;
+
         public MainWindow()
         {
             InitializeComponent();
+            mcolor = new ColorRGB();
+            mcolor.red = 0;
+            mcolor.green = 0;
+            mcolor.blue = 0;
+            this.lbl1.Background = new SolidColorBrush(Color.FromRgb(mcolor.red, mcolor.green, mcolor.blue));
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -38,9 +47,16 @@ namespace PZ_10_ex1
 
         private void btn_Save_Click(object sender, RoutedEventArgs e)
         {
-            string imgPath = @"D:\Lamaev\file.gif"; //Куда сохраняется файл
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Bitmap files (*.bmp)|*.bmp|Image files (*.jpg)|*.jpg";
+            if(save.ShowDialog() != true)
+            {
+                string fileName = save.FileName;
+                string imgPath = fileName; //Куда сохраняется файл
+            }
+
             MemoryStream ms = new MemoryStream();  //Поток памяти :)
-            FileStream fs = new FileStream(imgPath, FileMode.Create); //  Поток файла :)
+            FileStream fs = new FileStream(save.FileName, FileMode.Create); //  Поток файла :)
 
             //rtb - объект класса RenderTargetBitmap
             RenderTargetBitmap rtb = new RenderTargetBitmap((int)inkCanvas1.Width, (int)inkCanvas1.Height, 96, 96, PixelFormats.Default);
@@ -50,7 +66,60 @@ namespace PZ_10_ex1
             gifEnc.Frames.Add(BitmapFrame.Create(rtb));
             gifEnc.Save(fs);
             fs.Close();
-            MessageBox.Show("Файл сохранен, " + imgPath); //Для информации
+            MessageBox.Show("Файл сохранен, " + save.FileName); //Для информации
+        }
+        private void sld_Color_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var slider = sender as Slider;
+            string crlName = slider.Name; //Определяем имя контрола, который покрутили
+            double value = slider.Value; // Получаем значение контрола
+                                         //В зависимости от выбранного контрола, меняем ту или иную компоненту и переводим ее в тип byte
+            if (crlName.Equals("sld_RedColor"))
+            {
+                mcolor.red = Convert.ToByte(value);
+            }
+            if (crlName.Equals("sld_GreenColor"))
+            {
+                mcolor.green = Convert.ToByte(value);
+            }
+            if (crlName.Equals("sld_BlueColor"))
+            {
+                mcolor.blue = Convert.ToByte(value);
+            }
+
+            //Задаем значение переменной класса clr 
+            clr = Color.FromRgb(mcolor.red, mcolor.green, mcolor.blue);
+            //Устанавливаем фон для контрола Label 
+            this.lbl1.Background = new SolidColorBrush(Color.FromRgb(mcolor.red, mcolor.green, mcolor.blue));
+            // Задаем цвет кисти для контрола InkCanvas
+            this.inkCanvas1.DefaultDrawingAttributes.Color = clr;
+        }
+
+        private void btn_Select_Click(object sender, RoutedEventArgs e)
+        {
+            this.inkCanvas1.EditingMode = InkCanvasEditingMode.Select;
+        }
+
+        private void btn_Select_ink(object sender, RoutedEventArgs e)
+        {
+            this.inkCanvas1.EditingMode = InkCanvasEditingMode.Ink;
+        }
+
+        private void btn_AddText_Click(object sender, RoutedEventArgs e)
+        {
+            //Инициализация контрола tb типа TextBox
+            TextBox tb = new TextBox
+            {
+                Width = 100,
+                Height = 50,
+                BorderThickness = new Thickness(1),
+                BorderBrush = new SolidColorBrush(Color.FromRgb(5, 5, 5)),
+                Margin = new Thickness(20, 20, 0, 0)
+            };
+            //Добавление контрола tb
+            this.inkCanvas1.Children.Add(tb);
+            //Переключение фокуса на элемент, чтоб можно было сразу ввести текст с клавиатуры
+            tb.Focus();
         }
     }
 }
